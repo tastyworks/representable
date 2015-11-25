fd804d94f56724dc2d936c7d0bfe49068f5f9047
Nick Sutterer <apotonick@gmail.com>
2015-11-26 09:04:37 +1100

    cleanup tests. does it have to bee **o instead of ** in ruby 2.0?

--- test/instance_test.rb
+++ test/instance_test.rb
@@ -10,7 +10,7 @@ class InstanceTest < BaseTest
 
   describe "lambda { fragment } (new way of class: lambda { nil })" do
     representer! do
-      property :title, :instance  => lambda { |options| options[:fragment] }
+      property :title, instance: ->(fragment:nil, **) { fragment }
     end
 
     it "skips creating new instance" do
@@ -75,27 +75,6 @@ class InstanceTest < BaseTest
       from_hash({"song" => {"id" => 1}}, user_options: { volume: 1 }).song.args.must_equal([{"id"=>1}, {:volume=>1}]) }
   end
 
-  # TODO: raise and test instance:{nil}
-  # describe "property with instance: { nil }" do # TODO: introduce :representable option?
-  #   representer!(:inject => :song_representer) do
-  #     property :song, :instance => lambda { |*| nil }, :extend => song_representer
-  #   end
-
-  #   let (:hit) { hit = OpenStruct.new(:song => song).extend(representer) }
-
-  #   it "calls #to_hash on song instance, nothing else" do
-  #     hit.to_hash.must_equal("song"=>{"title"=>"Resist Stance"})
-  #   end
-
-  #   it "calls #from_hash on the existing song instance, nothing else" do
-  #     song_id = hit.song.object_id
-  #     hit.from_hash("song"=>{"title"=>"Suffer"})
-  #     hit.song.title.must_equal "Suffer"
-  #     hit.song.object_id.must_equal song_id
-  #   end
-  # end
-
-  # lambda { |fragment, i, Context(binding: <..>, args: [..])| }
 
   describe "sync" do
     representer!(:inject => :song_representer) do
